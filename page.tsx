aae4b1353455d45a65e6d978763e04dee3aad3e1
vrushanka1399 <vrushanka.1si17cs132@gmail.com>
2026-02-15 10:41:14 +0530

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,5 +1,29 @@
-export default function Home() {
-  return (
-    <h1>Welcome to the Products Store</h1>
-  );
+import { cookies } from "next/headers";
+import { verifyToken } from "@/lib/jwt";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+
+  if (!token) {
+    redirect("/login");
+  }
+
+  try {
+    const user = await verifyToken(token);
+
+    return (
+      <div>
+        <h1>Welcome {user.username}</h1>
+        <p>This is a protected dashboard.</p>
+
+        <form action="/api/logout" method="POST">
+          <button type="submit">Logout</button>
+        </form>
+      </div>
+    );
+  } catch {
+    redirect("/login");
+  }
 }
